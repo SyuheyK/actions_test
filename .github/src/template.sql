22406111865166a962a001a674c6f3b4e60cb6a0
SyuheyK <yfa72451@gmail.com>
2021-11-27 20:40:33 +0900

    move source files

--- .github/src/template.sql
+++
@@ -1,7 +0,0 @@
-DECLARE condition STRING DEFAULT '{condition_string1}';
-
-SELECT
-  {column_names}
-FROM `table_{table_name}`
-WHERE condition_column1 = condition
-  AND condition_column2 in ({condition_string1})
