ca2f24b8dcf4fa9f38655038af4b04abbc86305a
SyuheyK <yfa72451@gmail.com>
2021-11-27 19:22:40 +0900

    modified add_comment_to_issue.yaml and add python source files

---
+++ .github/src/template.sql
@@ -0,0 +1,7 @@
+DECLARE condition STRING DEFAULT '{condition_string1}';
+
+SELECT
+  {column_names}
+FROM `table_{table_name}`
+WHERE condition_column1 = condition
+  AND condition_column2 in ({condition_string1})
