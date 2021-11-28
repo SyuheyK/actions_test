64f5866323ea097f47f6732b9789f301660a58b6
SyuheyK <yfa72451@gmail.com>
2021-11-28 13:09:03 +0900

    modify add_comment_to_issue.yaml and source file

--- src/convert_sql.py
+++ src/convert_sql.py
@@ -1,8 +1,7 @@
 import argparse
 import json
 parser = argparse.ArgumentParser()
-parser.add_argument("template_sql", nargs = 1, help="template SQL for modification.", type=str)
-parser.add_argument("json_schema", nargs = 2, help="JSON file which specify the table schema for the output SQL.", type=str)
+parser.add_argument("json_schema", help="JSON file which specify the table schema for the output SQL.", type=str)
 args = parser.parse_args()
 
 with open(args.json_schema) as f:
@@ -18,6 +17,18 @@ for i in range(1,len(jsn)-1):
 condition_string2_list = list(dict.fromkeys(condition_string2_list))
 column_name_list = list(dict.fromkeys(column_name_list))
 
-query = args.template_sql.format(table_name=jsn['table_name'], condition_string1=jsn['condition_string1'], column_names = ", ".join(column_name_list), event_names = ", ".join(condition_string2_list))
+query = """
+DECLARE condition STRING DEFAULT '{condition_string1}';
+
+SELECT
+  {column_names}
+FROM `table_{table_name}`
+WHERE condition_column1 = condition
+  AND condition_column2 in ({condition_string2})
+""".format(table_name=jsn['table_name'], 
+      condition_string1=jsn['condition_string1'], 
+      column_names = ", ".join(column_name_list), 
+      event_names = ", ".join(condition_string2_list)
+)
 
 print("::set-output name=test::" + query)
