55f967de33d86da6c2b16b3019846dd655175619
SyuheyK <yfa72451@gmail.com>
2021-11-28 12:20:38 +0900

    modify python source file

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import json
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
+parser.add_argument("echo", help="echo the string you use here", type=str)
 args = parser.parse_args()
 
 print("::set-output name=test::" + args.echo)
