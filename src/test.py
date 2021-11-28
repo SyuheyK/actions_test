23156c0e62f05c4cc789b02c52f32366d2219927
SyuheyK <yfa72451@gmail.com>
2021-11-28 13:55:32 +0900

    source file

--- src/test.py
+++ src/test.py
@@ -4,6 +4,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("echo", help="echo the string you use here", type=str)
 args = parser.parse_args()
 
-test = open(args.echo, 'r').read().splitlines()
+test = open(args.echo, 'r').read().replace('\n', ' ')
 
 print("::set-output name=test::" + str(test))
